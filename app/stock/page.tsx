c8ba707dc298df2243fd685b25b3d0463edabef7
mukiwu <mukispace@gmail.com>
2023-12-04 13:58:52 +0800

    feat: 自製路由測試

--- app/stock/page.tsx
+++ app/stock/page.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+'use client'
+import React, { useEffect, useState } from 'react'
 
 export default function Page() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/fav')
+        const data = await response.json()
+        console.log('data', data)
+      } catch (error) {
+        console.error('Failed to fetch data:', error)
+      }
+    }
+    fetchData()
+  }, [])
+
   return <h1>所有股票</h1>
 }
