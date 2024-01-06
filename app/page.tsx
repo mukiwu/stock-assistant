ae83737c99ae48f8437ebc974c204b1e1f72b4d1
mukiwu <mukispace@gmail.com>
2024-01-06 22:52:27 +0800

    feat: 移除 table 資料

--- app/page.tsx
+++ app/page.tsx
@@ -6,33 +6,11 @@ import dayjs from 'dayjs'
 import theme from './global.theme.config'
 import Container from '@mui/material/Container'
 import { styled } from '@mui/material/styles'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell, { tableCellClasses } from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
 import { DataGrid, GridToolbar, GridCellParams, GridColDef, gridPageCountSelector, GridPagination, useGridApiContext, useGridSelector } from '@mui/x-data-grid'
 import LinearProgress from '@mui/material/LinearProgress'
 import { TablePaginationProps } from '@mui/material/TablePagination'
 import MuiPagination from '@mui/material/Pagination'
 import TSE from './component/TSE'
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.info.main,
-    color: theme.palette.secondary.main,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}))
-
-const CustomizedPagination = styled(MuiPagination)`
-  & .MuiPagination-ul {
-    flex-wrap: nowrap;
-  }
-`
 
 const getCellClassName = (params: GridCellParams) => {
   return `${Number(params.value) > 0 ? 'text-primary font-bold' : 'text-success font-bold'}`
@@ -41,7 +19,12 @@ const getCellClassName = (params: GridCellParams) => {
 const columns: GridColDef[] = [
   { field: 'symbol', headerName: '股票代碼', width: 100 },
   { field: 'name', headerName: '股票名稱', width: 150 },
-  { field: 'closePrice', headerName: '當前價', width: 120, type: 'number' },
+  {
+    field: 'closePrice', headerName: '當前價', width: 120, type: 'number',
+    // cellClassName: (params: GridCellParams) => {
+    //   return `${Number(params.value) > 0 ? 'text-primary font-bold' : 'text-success font-bold'}`
+    // }
+  },
   { field: 'openPrice', headerName: '開盤價', width: 120, type: 'number' },
   { field: 'highPrice', headerName: '最高價', width: 120, type: 'number' },
   { field: 'lowPrice', headerName: '最低價', width: 120, type: 'number' },
@@ -54,7 +37,7 @@ const columns: GridColDef[] = [
     }
   },
   {
-    field: 'changePercent', headerName: '漲跌幅', width: 120, headerAlign: 'right', align: 'right',
+    field: 'changePercent', headerName: '今日漲跌幅', width: 120, headerAlign: 'right', align: 'right',
     cellClassName: (params: GridCellParams) => getCellClassName(params),
     valueFormatter: (params) => {
       const valueFormatted = Number(params.value).toFixed(2)
@@ -63,16 +46,23 @@ const columns: GridColDef[] = [
   },
 ]
 
+const apiIntradayQuoteUrl = 'https://api.fugle.tw/marketdata/v1.0/stock/intraday/quote'
 const apiSnapshotQuotesUrl = 'https://api.fugle.tw/marketdata/v1.0/stock/snapshot/quotes'
 
 const Page = () => {
   const [hydrated, setHydrated] = useState(false)
   const [lastUpdated, setLastUpdated] = useState<Date>()
   const [quote, setQuote] = useState<any>([])
-  const [page, setPage] = useState(1)
+  const [page, setPage] = useState(0)
   const rowsPerPage = 15
 
-  const fetchSnapshot = async () => {
+  const fetchIntradayQuote = async () => {
+    const currentQuote = quote.slice((page - 1) * rowsPerPage, (page - 1) * rowsPerPage + rowsPerPage)
+    console.log(currentQuote)
+  }
+
+  const fetchSnapshot = async (): Promise<void> => {
+    console.log('fetchSnapshot')
     try {
       const headers = {
         'X-API-KEY': process.env.API_KEY as string
@@ -81,9 +71,11 @@ const Page = () => {
         headers: headers
       })
       const data = response.data.data
+      if (data.length === 0) return
       data.forEach((item: any) => {
         item.id = item.symbol
       })
+      setPage(1)
       setQuote(data)
     } catch (error) {
       console.error('API 請求錯誤:', error)
@@ -111,10 +103,6 @@ const Page = () => {
     return <GridPagination ActionsComponent={Pagination} {...props} />
   }
 
-  const handleChangePage = (_: React.ChangeEvent<unknown>, newPage: number) => {
-    setPage(newPage)
-  }
-
   useEffect(() => {
     setHydrated(true)
   }, [])
@@ -124,15 +112,18 @@ const Page = () => {
       fetchSnapshot()
       setLastUpdated(new Date())
     }
-
   }, [hydrated])
+
+  useEffect(() => {
+    fetchIntradayQuote()
+  }, [page])
 
   if (!hydrated) return null
 
   return (
     <>
       <TSE />
-      <Container maxWidth="xl">
+      <Container maxWidth="xl" className="mb-8">
         <div className="my-4 flex justify-between">
           <h1 className="text-lg">台股即時報價</h1>
           <div className="flex items-center text-sm">
@@ -164,38 +155,6 @@ const Page = () => {
           }}
           loading={quote.length === 0}
         />
-
-        <TableContainer>
-          <Table sx={{ minWidth: 650 }} size="small" aria-label="a quote table">
-            <TableHead>
-              <TableRow>
-                <StyledTableCell>股票代碼</StyledTableCell>
-                <StyledTableCell>股票名稱</StyledTableCell>
-                <StyledTableCell align="right">當前價</StyledTableCell>
-                <StyledTableCell align="right">開盤價</StyledTableCell>
-                <StyledTableCell align="right">最高價</StyledTableCell>
-                <StyledTableCell align="right">最低價</StyledTableCell>
-                <StyledTableCell align="right">今日漲跌</StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {(quote.slice((page - 1) * rowsPerPage, (page - 1) * rowsPerPage + rowsPerPage)).map((row: any) => (
-                <TableRow key={row.symbol}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">{row.symbol}</TableCell>
-                  <TableCell>{row.name}</TableCell>
-                  <TableCell align="right">{row.closePrice}</TableCell>
-                  <TableCell align="right">{row.openPrice}</TableCell>
-                  <TableCell align="right">{row.highPrice}</TableCell>
-                  <TableCell align="right">{row.lowPrice}</TableCell>
-                  <TableCell align="right">{row.change}({row.changePercent})</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <CustomizedPagination className="my-6 flex justify-center" count={Math.ceil(quote.length / rowsPerPage)} page={1} variant="outlined" shape="rounded" onChange={handleChangePage} />
       </Container>
     </>
   )
